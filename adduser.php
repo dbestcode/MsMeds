6690bac4b07230847a96b95f3574523ef81336aa
Nicholai Best <nicholai.best@gmail.com>
2022-02-03 13:25:43 -0500

    Added Auth code to protect from DDOS

--- adduser.php
+++ adduser.php
@@ -5,41 +5,33 @@ Page adds new users, currently no ddos protection
 session_start();
 require_once('./php/head.php');
 //if a barcode has not been sent, redirect barcode needed.
-if(isset($_GET["barcode"])){
-	//vaildidate that the barcode starts with MedId(no currently in use)
-	if (strpos($_GET["barcode"], "edId") == 1) {
-		$_SESSION["ncode"] = $_GET["barcode"];
-	} else {
-		//echo "invaild Barcode ID";
-		//header("Location: index.php");
-	}
-} else {
-	//echo "<meta http-equiv='refresh' content='1;url=index.php'>";
+foreach($_GET as $x =>$x_value){
+	echo $x . " " . $x_value . "<br>";
 }
 
 //catch submission
-if(isset($_GET['submit'])){
+if(isset($_POST['submit'])){
 	//$name = $_POST['id'];
 	$sql="INSERT INTO ";
 	$i=0;
 	$feilds="";
 	$values="";
- 	foreach($_GET as $x =>$x_value){
+ 	foreach($_POST as $x =>$x_value){
 		echo $i . ": " .$x . "=>" .  $x_value . "<br>";
                 if($i==0){
 			$sql=$sql . $x_value;
 		} elseif($i==1){
 			$feilds = $feilds . "id, ";
 			$values = $values . "NULL, ";
-		} elseif ($i==count($_GET)-3){
+		} elseif ($i==count($_POST)-3){
 			$feilds = $feilds . $x . ", ";
                 					//PIN, storing the hash not the pin
 			$values = $values . "'" . hash('md5',$x_value) . "', ";
-		} elseif ($i==count($_GET)-2){
+		} elseif ($i==count($_POST)-2){
                 	$feilds = $feilds . $x;		//last item, omit comma connting the string
 			$values = $values . "'" . $x_value . "'";
-		} elseif ($i==count($_GET)-1){
-							//sumit value is in the GET array, ignoring so not added to sql stament
+		} elseif ($i==count($_POST)-1){
+							//sumit value is in the POSt array, ignoring so not added to sql stament
 		} else {
 			$feilds = $feilds . $x . ", ";
 			$values = $values . "'" . $x_value . "', ";
@@ -47,30 +39,65 @@ if(isset($_GET['submit'])){
 		$i++;
 
         }
-	$sql= $sql . " (" . $feilds . ") VALUES (" . $values . ")";
-	/*echo "<br>" . $sql;
+/*	$sql= $sql . " (" . $feilds . ") VALUES (" . $values . ")";
+	echo "<br>" . $sql;
 	echo "<br>";
 	echo "Feilds:" . $feilds;
 	echo "<br>";
 	echo "Values" . $values;
 	echo $sql;
-	*/
+*/
 	include ('conn.php');
 	$result = $conn->query($sql);
 	echo $result;
 	$conn->close();
 	header("Location: logout.php");
 }
-
+/*
 if(isset($_GET["barcode"])){
 	if (strpos($_GET["barcode"], "edId") == 1) {//vaildidate that the barcode starts with MedId
-	$_SESSION["ncode"] = $_GET["barcode"];
+		$_SESSION["ncode"] = $_GET["barcode"];
 	} else {
 		//echo "invaild Barcode ID";
 		//header("Location: index.php");
 	}
 } else {
-//	header("Location: index.php");
+	header("Location: index.php");
+}*/
+function print_form(){
+	if(!(isset($_POST["authcode"]))){
+		echo "<form method='post' action='". htmlspecialchars($_SERVER['PHP_SELF']) . "'>";
+		echo "<H2>New User Authrization Code:</H2>";
+		echo "Please enter your authorization code:<table>";
+		echo "<tr><td colspan='2'><input type='text' name='authcode' value='SuperSim'><br></td></tr>";
+		echo "<input type='hidden' name='barcode' value='" . $_GET['barcode'] . "'><br></td></tr>";
+		echo "<tr>";
+		echo "<td><a href='index.php' style='background-color: #20285b;border: none; color: white; padding: 16px 32px; text-decoration: none;  margin: 4px 2px; cursor: pointer;'>Cancel</a></td>";
+		echo "<td><input type='submit' name='submita' value='Submit Form'><br></td></tr>";
+		echo "</table>";
+		echo "</form>";
+	} else if ($_POST["authcode"]=="SuperSim22"){
+		echo "<form method='post' action='". htmlspecialchars($_SERVER['PHP_SELF']) . "'>";
+		echo "<H2>Register New User</H2>";
+		echo "<table>";
+		echo "<form method='post' action=/additem.php>";
+		echo "<input type='hidden' name=table value=users>";
+		echo "<input type='hidden' name='id'>";
+		echo "<tr><td>FirstName</td><td><input type='text' name='FirstName' autofocus><br></td></tr>";
+		echo "<tr><td>LastName</td><td><input type='text' name='LastName'><br></td></tr>";
+		echo "<tr><td>Barcode</td><td><input type='text' readonly='true' name='Barcode' value='" . $_POST['barcode'] . "'><br></td></tr>";
+		echo "<tr><td>Pin</td><td><input type='text' name='Pin'><br></td></tr>";
+		echo "<tr><td>AccessLevel</td><td><input type='text' readonly='true' name='AccessLevel' value='1'><br></td></tr>";
+		echo "<tr>";
+		echo "<td><a href='index.php' style='background-color: #20285b;border: none; color: white; padding: 16px 32px; text-decoration: none;  margin: 4px 2px; cursor: pointer;'>Cancel</a></td>";
+		echo "<td><input type='submit' name='submit' value='Submit Form'><br></td></tr>";
+		echo "</table>";
+		echo "</form>";
+
+	}else{
+		echo "NOT A VALID CODE<br>";
+		echo "<meta http-equiv='refresh' content='1;url=index.php'>";
+	}
 }
 ?>
 <!DOCTYPE html>
@@ -83,31 +110,8 @@ if(isset($_GET["barcode"])){
 <?php require "./php/title.php" ?>
 <div class='container' style="height:500px">
 	<div class='vhcenter'>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-		<H2>Register New User</H2>
-		<table>
-		<form method='post' action=/additem.php>
-		<input type='hidden' name=table value=users>
-		<input type='hidden' name='id'>
-		<tr><td>FirstName</td><td><input type='text' name='FirstName' autofocus><br></td></tr>
-		<tr><td>LastName</td><td><input type='text' name='LastName'><br></td></tr>
-		<tr><td>Barcode</td><td><input type='text' readonly='true' name='Barcode' value='<?PHP echo $_GET["barcode"]; ?>'><br></td></tr>
-		<tr><td>Pin</td><td><input type='text' name='Pin'><br></td></tr>
-		<tr><td>AccessLevel</td><td><input type='text' readonly='true' name='AccessLevel' value='1'><br></td></tr>
-		<tr>
-		<td><a href='index.php' style='
-				  background-color: #20285b;
-				  border: none;
-				  color: white;
-				  padding: 16px 32px;
-				  text-decoration: none;
-				  margin: 4px 2px;
-				  cursor: pointer;
-				'>Cancel</a></td>
-		<td><input type='submit' name='submit' value='Submit Form'><br></td></tr>
-		</table>
-		</form>
-</div>
+		<?php print_form(); ?>
+	</div>
 </div>
 <?php require './php/footer.php';?>
 </body>
