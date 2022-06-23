e0c9711406d1ff163a0a7fc0e3f34894ae54e99d
Nicholai Best <nicholai.best@gmail.com>
2022-06-23 12:50:34 -0400

    conn.php, needs a UN and PW for mysql durning setup

--- conn.php
+++ conn.php
@@ -1,10 +1,13 @@
 <?php
-//php login details, badly needs seccurity audit
-//include "cherry.php";
 $servername = "localhost";
-$username = "simlab";
-$password = '$imlab4ever';
+
+# setup login credientials and remove the notice below,
+# sql user is what was setup at time for installation of sql.
+echo '<script type="text/javascript">window.onload = function () { alert("MSMEDS SETUP INCOMPLETE, SQL LOGIN CREDENTIALS NOT SET!!! SET IN conn.php"); }</script>';
+$username = ""; #Add me!
+$password = ""; #Add me!
 $dbname = "sudo_meds";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
