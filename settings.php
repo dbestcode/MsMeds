aa1bc9749976a6f93dffdcb83ec3c95a5f4b9e33
Nicholai Best <nicholai.best@gmail.com>
2023-01-12 16:48:20 -0500

    Upload mutiple files for a patient on a page part 1

--- settings.php
+++ settings.php
@@ -9,16 +9,16 @@ define('PROJECT_VERSION','1.1');
 // Values for $_POST['originForm']
 // Used for detemining what data is being sent
 define('FRM_VIEW_TABLE',0);   // View a table
-define('FRM_EDIT_ITEM',1);          // Edit an Item
-define('FRM_DEL_ITEM',2); // Delete an item
-define('FRM_NEW_ITEM',3); // Delete an item
+define('FRM_SELECT_ITEM',1);          // Edit an Item
+define('FRM_UPDATE_ITEM',2);          // Edit an Item
+define('FRM_DEL_ITEM',3); // Delete an item
+define('FRM_NEW_ITEM',4); // Delete an item
 
 // Values for $_POST['selectedTable']
-define('TBL_PATIENTS',0);
-define('TBL_USERS',1);
-define('TBL_DRUGS',2);
-define('TBL_IO_REPORT',3);
-
+define('TBL_PATIENTS','patients');
+define('TBL_USERS','users');
+define('TBL_DRUGS','drugs');
+define('TBL_IO_REPORT','io_report');
 
 require_once('./common-items.php');
 
@@ -33,9 +33,15 @@ if (isset($_POST['originForm'])){
     pageViewTable();
     exit;
     break;
-  case FRM_EDIT_ITEM:
+  case FRM_SELECT_ITEM:
   // What am I?
-    pageEditRow();
+    pageSelectItem();
+    exit;
+    break;
+  case FRM_UPDATE_ITEM:
+  // What am I?
+    pageUpdateRow();
+    pageSelectItem();
     exit;
     break;
   case FRM_DEL_ITEM:
@@ -108,7 +114,7 @@ function pageViewTable(){
     echo "<th>EDIT</th>";
 
     echo "<form action='".$_SERVER['PHP_SELF']."' method='post'>
-    <input type='hidden' name='originForm' value='".FRM_EDIT_ITEM."'>
+    <input type='hidden' name='originForm' value='".FRM_SELECT_ITEM."'>
     <input type='hidden' name='selectedTable' value='".$_POST["selectedTable"]."'><br />";
     for($r=0;$r<count($table);$r++){
       echo "<tr>";
@@ -133,7 +139,6 @@ function pageViewTable(){
 		header("Location: additem.php");
 
 */
-
 
 function pageMainMenu(){
 
@@ -244,57 +249,13 @@ echo "
 
 }
 
-function pageEditRow(){
-switch ($_POST["selectedTable"]){
-  case TBL_DRUGS:
-    $strSelectedTable='drugs';
-    break;
-  case TBL_USERS:
-    $strSelectedTable='users';
-    break;
-  case TBL_PATIENTS:
-    $strSelectedTable='patients';
-    break;
-  case TBL_IO_REPORT:
-    $strSelectedTable='io_report';
-    break;
-}
-
-/*if(isset($_POST['submit']))
-{
-	$sql="UPDATE ";
-	$i=0;
- 	foreach($_POST as $x =>$x_value){
-                if($i==0){
-			$sql=$sql . $x_value . " SET ";
-		} elseif($i==1){
-			$id=$x_value;
-		} elseif($i==2){
-                	$sql = $sql . $x . " = '" . $x_value ."' ";
-		} elseif ($i==count($_POST)-1){
-                	echo "'REMOVED ";
-		} else {
-                	$sql = $sql . ", " . $x . " = '" . $x_value ."' ";
-	                echo "</tr>\n";
-		}
-		$i++;
-
-        }
-	$sql=$sql . "WHERE id=" . $id;
-	include ('conn.php');
-	$result = $conn->query($sql);
-	$conn->close();
-	unset($_SESSION["edititem"]);
-	header("Location: displaytable.php");
-	exit;
-}*/
-
+function pageSelectItem(){
 
 echo getHead('Settings',LAST_WORK,'').getTitle('');
 
 $conn = ConnectDB();
 
-$sql = "SELECT * FROM $strSelectedTable WHERE id=" . $_POST["itemIndex"];
+$sql = "SELECT * FROM ".$_POST["selectedTable"]." WHERE id=" . $_POST["itemIndex"];
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 	//load data into 'table[][]'
@@ -302,15 +263,17 @@ if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
 		$table[$i]=$row;
 		$i++;
+    echo $i . "<br>";
 	}
 		//List keys and print as table headers
 	echo "
-  <form method='post' action=". htmlspecialchars($_SERVER['PHP_SELF']) . ">";
+  <form name ='FRMEDIT' method='post' action=". htmlspecialchars($_SERVER['PHP_SELF']) . ">";
 	echo "
   <table>";
 	echo "<tr>
   ";
 	echo tablecell("<input type='hidden' name='selectedTable' value=".$_POST["selectedTable"]."><br>");
+  echo tablecell("<input type='hidden' name='originForm' value='".FRM_UPDATE_ITEM."'><br />");
   echo tablecell("<input type='hidden' name='itemIndex' value=".$_POST["itemIndex"]."><br>");
 	echo "</tr>
   ";
@@ -323,36 +286,18 @@ if ($result->num_rows > 0) {
 			echo "</tr>\n";
 			break;
     case "MarFile":
-			echo "<tr>";
-			echo tablecell($x);
-			echo tablecell("<input list='uploads' name='" . $x . "' value='" . $x_value . "'  style='width:300px'><br>");
-      echo tablecell("<input type='file' name='fileToUpload' id='fileToUpload'>");
-			echo "</tr>\n";
-			break;
 		case "HpFile":
-			echo "<tr>";
-			echo tablecell($x);
-			echo tablecell("<input list='uploads' name='" . $x . "' value='" . $x_value . "'  style='width:300px'><br>");
-      echo tablecell("<input type='file' name='fileToUpload' id='fileToUpload'>");
-			echo "</tr>\n";
-			break;
 		case "OrdersFile":
-			echo "<tr>";
-			echo tablecell($x);
-			echo tablecell("<input list='uploads' name='" . $x . "' value='" . $x_value . "'  style='width:300px'><br>");
-      echo tablecell("<input type='file' name='fileToUpload' id='fileToUpload'>");
-			echo "</tr>\n";
-			break;
 		case "ReportFile":
 			echo "<tr>";
-			echo tablecell($x);
-			echo tablecell("<input list='uploads' name='" . $x . "' value='" . $x_value . "' style='width:300px'><br>");
-      echo tablecell("<input type='file' name='fileToUpload' id='fileToUpload'>");
+			echo tablecell($x.": ");
+			echo tablecell("<a href='./patient_files/".$x_value . "' target='_blank'>$x_value</a>");
+      echo tablecell("<input type='file' name='$x'>");
 			echo "</tr>\n";
 			break;
 		default:
 			echo "<tr>";
-			echo tablecell($x);
+			echo tablecell($x.": ");
 			echo tablecell("<input type='text' name='" . $x . "' value='" . $x_value . "'><br>");
 			echo "</tr>\n";
 		}
@@ -365,9 +310,59 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 echo getTail();
-
-
-
 }
 
+function pageUpdateRow(){
+  //get the table row to be updated
+  $conn = ConnectDB();
+  $sql = "SELECT * FROM ".$_POST["selectedTable"]." WHERE id=" . $_POST["itemIndex"];
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    $TableRow = $result->fetch_assoc();
+  }
+  //beging consrtuing sql statment to update the record
+	$sql="UPDATE ".$_POST["selectedTable"] . " SET ";
+	$i=0;
+ 	foreach($_POST as $x =>$x_value){
+    //check for blank file submission for each pdf
+    //if so dont update the value 
+    switch($x){
+    case "MarFile":
+    case "HpFile":
+    case "OrdersFile":
+    case "ReportFile":
+      if ($x_value == ''){
+        $x_value = $TableRow[$x];
+      } else {
+        /*code to remove an old file if a new one is submitted
+         * will need code to upload here as well
+         * $file = fopen($_POST["file"],"w");
+  fwrite($file,"Hello World. Testing!");
+  fclose($file);
+  unlink($_POST["fileToDelete"]);*/
+      }
+      break;
+    }
+    
+    if($i==0){
+    } elseif ($i==count($_POST)-1){ //skip all these as they info about table and form
+		} elseif($i==1){
+		} elseif($i==2){
+    } elseif($i==3){
+    } elseif($i==4){
+                	$sql = $sql . $x . " = '" . $x_value ."' ";
+		} else {
+                	$sql = $sql . ", " . $x . " = '" . $x_value ."'";
+		}
+		$i++;
+  }
+	$sql=$sql . " WHERE id = " . $_POST["itemIndex"];
+	$result = $conn->query($sql);
+	$conn->close();
+  //INSERT INTO `patients` (`id`, `FirstName`, `LastName`, `Barcode`, `DOB`, `Immortal`, `Provider`, `MarFile`, `HpFile`, `OrdersFile`, `ReportFile`) 
+  //                VALUES (NULL, 'name', 'name', '5435943', '01/23/1059', 'a', 'Aldo Castaneda M.D.', 'blank.pdf', 'blank.pdf', 'blank.pdf', 'blank.pdf')
+  
+  //UPDATE `patients` SET `FirstName` = 'Juanita1', `LastName` = 'Ibarraa',
+  //       `Barcode` = '84956224d', `DOB` = '1242', `HpFile` = 'd', `OrdersFile` = 's' WHERE `patients`.`id` = 23 
+}
 ?>
